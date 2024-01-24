7d34943dac4b32a501b9041c5968b7293ff4eb55
Nano <kaweepatinn1@gmail.com>
2024-01-24 10:05:37 +0800

    remove redundant import

--- src/SimpleRhythmGame/Database.java
+++ src/SimpleRhythmGame/Database.java
@@ -5,7 +5,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.UUID;
 
 public class Database {
